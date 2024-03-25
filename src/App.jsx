7a86fb5aea07d6c642193c4c502a01ca9a451ba0
unknown <misyantoninjerry@gmail.com>
2024-03-25 15:45:57 +0300

    Premier commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='grid grid-cols-3 gap-4 flex'>
+      <div className='bg-red-500'>
+        <h1>1</h1>
+      </div>
+      <div className='bg-slate-800'>
+        <h1>2</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
